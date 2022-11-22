e2d7cd07300eb70ee5c1d5c2143685ae0579aa96
Felix Dion <felix.dion96@hotmail.com>
2022-11-22 11:22:07 -0500

    test branche

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -16,7 +16,7 @@ function attacherListenerMenuGauche(){
         });
     }
 }
-
+/*test*/
 /**
  * Fonction qui initie le lancement des fonctions de ce script. Appelée par "chargerSousContenu" dans navigation.js.
  * Remplace le DOMContentLoaded qui est lancé bien avant que le contenu associé à ce script ne soit dans l'écran.
