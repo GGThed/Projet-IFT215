b1482da62b6feb8eba6c6172d0dbf1b755777b45
GGThed <115330770+GGThed@users.noreply.github.com>
2022-11-30 16:53:01 -0500

    Add files via upload

--- routes/html.js
+++ routes/html.js
@@ -30,4 +30,12 @@ router.get('/panier', function(req, res){
     res.sendFile(path.join(__dirname + '/../client/panier.html'));
 });
 
+router.get('/paiement', function(req, res){
+    res.sendFile(path.join(__dirname + '/../client/paiement.html'));
+});
+
+router.get('/confirmation', function(req, res){
+    res.sendFile(path.join(__dirname + '/../client/confirmation.html'));
+});
+
 module.exports = router
